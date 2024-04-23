a4f404fe5b188b187df2e7b6a5b22bdd8140fef5
henry <Hd116527@gmail.com>
2024-04-22 22:49:47 -0500

    various changes for the sake of NDC

--- SDL_helper.c
+++ SDL_helper.c
@@ -13,19 +13,73 @@ int main(){
     //hardcoding option
     //rgbArray* vals = load();
     vertexBuffer* vb = malloc(sizeof(vertexBuffer));
-    static int verticesArray[] = {
+    // static int verticesArray[] = {
 
-    400, 300, 200, // Vertex 1
-    600, 300, 100, // Vertex 2
-    500, 500, 300, // Vertex 3 (apex)
+    // 400, 300, 200, // Vertex 1
+    // 600, 300, 100, // Vertex 2
+    // 500, 500, 300, // Vertex 3 (apex)
 
-    600, 300, 200, // Vertex 1 (shared with Triangle 1)
-    800, 300, 100, // Vertex 2
-    700, 500, 300  // Vertex 3 (apex)
+    // 600, 300, 200, // Vertex 1 (shared with Triangle 1)
+    // 800, 300, 100, // Vertex 2
+    // 700, 500, 300  // Vertex 3 (apex)
+    // };
+    float cubeVertices[] = {
+    // Bottom face (two triangles)
+    -1.0f, -1.0f, -1.0f,  // Triangle 1, Vertex 1
+     1.0f, -1.0f, -1.0f,  // Triangle 1, Vertex 2
+     1.0f, -1.0f,  1.0f,  // Triangle 1, Vertex 3
+    
+     1.0f, -1.0f,  1.0f,  // Triangle 2, Vertex 1
+    -1.0f, -1.0f,  1.0f,  // Triangle 2, Vertex 2
+    -1.0f, -1.0f, -1.0f,  // Triangle 2, Vertex 3
+
+    // Top face (two triangles)
+    -1.0f,  1.0f, -1.0f,  // Triangle 1, Vertex 1
+     1.0f,  1.0f, -1.0f,  // Triangle 1, Vertex 2
+     1.0f,  1.0f,  1.0f,  // Triangle 1, Vertex 3
+    
+     1.0f,  1.0f,  1.0f,  // Triangle 2, Vertex 1
+    -1.0f,  1.0f,  1.0f,  // Triangle 2, Vertex 2
+    -1.0f,  1.0f, -1.0f,  // Triangle 2, Vertex 3
+
+    // Front face (two triangles)
+    -1.0f, -1.0f,  1.0f,
+     1.0f, -1.0f,  1.0f,
+     1.0f,  1.0f,  1.0f,
+
+     1.0f,  1.0f,  1.0f,
+    -1.0f,  1.0f,  1.0f,
+    -1.0f, -1.0f,  1.0f,
+
+    // Back face (two triangles)
+    -1.0f, -1.0f, -1.0f,
+     1.0f, -1.0f, -1.0f,
+     1.0f,  1.0f, -1.0f,
+
+     1.0f,  1.0f, -1.0f,
+    -1.0f,  1.0f, -1.0f,
+    -1.0f, -1.0f, -1.0f,
+
+    // Left face (two triangles)
+    -1.0f, -1.0f, -1.0f,
+    -1.0f, -1.0f,  1.0f,
+    -1.0f,  1.0f,  1.0f,
+
+    -1.0f,  1.0f,  1.0f,
+    -1.0f,  1.0f, -1.0f,
+    -1.0f, -1.0f, -1.0f,
+
+    // Right face (two triangles)
+     1.0f, -1.0f, -1.0f,
+     1.0f, -1.0f,  1.0f,
+     1.0f,  1.0f,  1.0f,
+
+     1.0f,  1.0f,  1.0f,
+     1.0f,  1.0f, -1.0f,
+     1.0f, -1.0f, -1.0f
     };
-
-    vb->length = 18;
-    vb->vertices = verticesArray;
+    vb->length = 108;
+    vb->vertices = cubeVertices;
     framebuffer* fb = createFrameBuffer(1000, 700);
 
     SDL_Window* window;    
@@ -49,10 +103,7 @@ int main(){
 
     SDL_UpdateTexture(texture, NULL, fb->pixels, fb->width * 3);
 
-    matrix4x4 rotationMatrix, translationMatrix, perspectiveProjectionMatrix;
-    createRotationMatrixX(60.0, rotationMatrix);
-    createTranslationMatrix(0.0, 0.0, 0.0, translationMatrix);
-    createPerspectiveProjectionMatrix(60.0, 1.0, 100.0, 1000.0/700.0, perspectiveProjectionMatrix);
+    
     
     //the vertex buffer that is actually altered.
     vertexBuffer* framevb = malloc(sizeof(vertexBuffer));
@@ -60,10 +111,17 @@ int main(){
     framevb->vertices = malloc(sizeof(int) * vb->length);
 
     //movement variables
-    float mx = 300;
+    float mx = 0;
     float my = 0;
     float mz = 0;
     float angle = 0.0;
+
+    //update matrices
+    matrix4x4 rotationMatrix, translationMatrix, scalingMatrix, perspectiveProjectionMatrix;
+    //createPerspectiveProjectionMatrix(60.0, 1.0, 100.0, 1000.0/700.0, perspectiveProjectionMatrix);
+    createScalingMatrix(0.5f, scalingMatrix);
+    createTranslationMatrix(mx, my, mz, translationMatrix);
+    createRotationMatrixX(angle, rotationMatrix);
 
     int quit = 0;
     SDL_Event e;
@@ -75,17 +133,16 @@ int main(){
         }
         //input gathering
         const Uint8* keystate = SDL_GetKeyboardState(NULL);
-        if(keystate[SDL_SCANCODE_W]){mz += 1.0f;}
-        if(keystate[SDL_SCANCODE_A]){mx -= 1.0f;}
-        if(keystate[SDL_SCANCODE_S]){mz -= 1.0f;}
-        if(keystate[SDL_SCANCODE_D]){mx += 1.0f;}
-        if(keystate[SDL_SCANCODE_LSHIFT]){ my -= 1.0f;}
-        if(keystate[SDL_SCANCODE_SPACE]){ my += 1.0f;}
+        if(keystate[SDL_SCANCODE_W]){mz += 0.01f;}
+        if(keystate[SDL_SCANCODE_A]){mx -= 0.01f;}
+        if(keystate[SDL_SCANCODE_S]){mz -= 0.01f;}
+        if(keystate[SDL_SCANCODE_D]){mx += 0.01f;}
+        if(keystate[SDL_SCANCODE_LSHIFT]){ my -= 0.01f;}
+        if(keystate[SDL_SCANCODE_SPACE]){ my += 0.01f;}
+
         if(keystate[SDL_SCANCODE_D]){angle += 0.1;}
         if(keystate[SDL_SCANCODE_A]){angle -= 0.1;}
-        //update matrices
-        createTranslationMatrix(mx, my, mz, translationMatrix);
-        createRotationMatrixX(angle, rotationMatrix);
+        
 
 
         //linear operations
@@ -96,20 +153,23 @@ int main(){
             temp.z = (float)vb->vertices[i + 2];
             temp.w = 1.0f;
             //printf("old: %f, %f , %f\n", temp.x, temp.y, temp.z);
-            //vecByMatrix4x4(&temp, translationMatrix);
-            vecByMatrix4x4(&temp, rotationMatrix);
-            //printf("new: %f, %f , %f\n", temp.x, temp.y, temp.z);
+            vecByMatrix4x4(&temp, translationMatrix);
+            vecByMatrix4x4(&temp, scalingMatrix);
+            //createRotationMatrix(angle, translationMatrix);
+            createTranslationMatrix(mx, my, mz, translationMatrix);
+            //vecByMatrix4x4(&temp, rotationMatrix);
             //vecByMatrix4x4(&temp, perspectiveProjectionMatrix);
+
             
             //perspective divide
             // temp.x = temp.x / temp.w;
             // temp.y = temp.y / temp.w;
             // temp.z = temp.z / temp.w;
-            
+            //printf(": %f, %f, %f :", temp.x, temp.y, temp.z);
             //create the temporary VBO
-            framevb->vertices[i] = (int)(temp.x + 0.5f) ;
-            framevb->vertices[i + 1] = (int)(temp.y + 0.5f);
-            framevb->vertices[i + 2] = (int)(temp.z + 0.5f);
+            framevb->vertices[i] = temp.x;
+            framevb->vertices[i + 1] = temp.y;
+            framevb->vertices[i + 2] = temp.z;
         }
 
         //build framebuffer
--- graphicsEnginePrivateFunctions.h
+++ graphicsEnginePrivateFunctions.h
@@ -17,7 +17,7 @@ normalBuffer* generateNormals(vertexBuffer* vb){
         vec2.y = vb->vertices[i + 4];
         vec2.z = vb->vertices[i + 5];
 
-        vecResult = crossProduct(vec1, vec2);
+        vec3 vecResult = crossProduct(vec1, vec2);
 
         nb->normals[i] = vecResult.x;
         nb->normals[i + 1] = vecResult.y;
@@ -25,29 +25,30 @@ normalBuffer* generateNormals(vertexBuffer* vb){
 
         nb->normals[i + 3] = vecResult.x;
         nb->normals[i + 4] = vecResult.y;
-        nb->normals[i + 5] = vecResult.z 
+        nb->normals[i + 5] = vecResult.z;
 
         nb->normals[i + 6] = vecResult.x;
         nb->normals[i + 7] = vecResult.y;
         nb->normals[i + 8] = vecResult.z;
     }
+    return nb;
 }
 
 //might do this differently
 //may be better to just have a matrix associated with an obj
 void objectSpaceToWolrdSpace(vertexBuffer* vb, int scalar){
-    for(int i = 0; i< vb.length; i+= 3){
+    for(int i = 0; i< vb->length; i+= 3){
         vec4 temp;
-        matrix4x4 mScalar
-        temp.x = vb->pixels[i];
-        temp.y = vb->pixels[i + 1];
-        temp.z = vb->pixels[i + 2];
+        matrix4x4 mScalar;
+        temp.x = vb->vertices[i];
+        temp.y = vb->vertices[i + 1];
+        temp.z = vb->vertices[i + 2];
         temp.w = 1;
-        matrix4x4 = createScalingMatrix(scalar, mScalar);
-        vecByMatrix4x4(&temp, &mScalar);
-        vb->pixels[i] = temp.x;
-        vb->pixels[i + 1] temp.y;
-        vb->pixels[i + 2] = temp.z;
+        createScalingMatrix(scalar, mScalar);
+        vecByMatrix4x4(&temp, mScalar);
+        vb->vertices[i] = temp.x;
+        vb->vertices[i + 1] = temp.y;
+        vb->vertices[i + 2] = temp.z;
     }
 
 }
